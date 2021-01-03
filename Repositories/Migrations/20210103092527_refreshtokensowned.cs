b8d1a94c1af4532f6b273ebbf8061cfa9fb865cb
titnet4 <titnet4@gmail.com>
2021-01-03 17:57:17 +0600

    updated migrations file

---
+++ Repositories/Migrations/20210103092527_refreshtokensowned.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repositories.Migrations
+{
+    public partial class refreshtokensowned : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshToken_Users_UserId",
+                table: "RefreshToken");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "RefreshToken",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshToken_Users_UserId",
+                table: "RefreshToken",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshToken_Users_UserId",
+                table: "RefreshToken");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "RefreshToken",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshToken_Users_UserId",
+                table: "RefreshToken",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
